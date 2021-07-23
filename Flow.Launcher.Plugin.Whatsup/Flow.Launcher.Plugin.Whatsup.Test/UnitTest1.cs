29b28e013e189646393fece18d874e8f5a95a84b
a <t>
2021-07-24 01:26:06 +0300

    updates

---
+++ Flow.Launcher.Plugin.Whatsup/Flow.Launcher.Plugin.Whatsup.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Flow.Launcher.Plugin.Whatsup.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
