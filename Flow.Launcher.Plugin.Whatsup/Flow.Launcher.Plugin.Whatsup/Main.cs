c2b59a81ee74e1773514ebe176f127f110772c17
a <t>
2021-07-24 01:10:56 +0300

    initial start

---
+++ Flow.Launcher.Plugin.Whatsup/Flow.Launcher.Plugin.Whatsup/Main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flow.Launcher.Plugin.Whatsup
+{
+    public class Main : IPlugin
+    {
+        public List<Result> Query(Query query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Init(PluginInitContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
