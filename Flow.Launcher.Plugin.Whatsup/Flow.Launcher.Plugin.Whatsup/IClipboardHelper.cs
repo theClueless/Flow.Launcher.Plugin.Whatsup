4aad7af2ce93c618528e8dc3a9fce0331296f95e
a <t>
2021-07-24 01:23:25 +0300

    initial

---
+++ Flow.Launcher.Plugin.Whatsup/Flow.Launcher.Plugin.Whatsup/IClipboardHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+
+namespace Flow.Launcher.Plugin.Whatsup
+{
+    public interface IClipboardHelper
+    {
+        string GetClipboardText();
+    }
+
+    public class ClipboardHelper : IClipboardHelper
+    {
+        public string GetClipboardText()
+        {
+            Exception threadEx = null;
+            var text = "";
+            var staThread = new Thread(
+                delegate ()
+                {
+                    try
+                    {
+                        text = Clipboard.GetText();
+                    }
+
+                    catch (Exception ex)
+                    {
+                        threadEx = ex;
+                    }
+                });
+            staThread.SetApartmentState(ApartmentState.STA);
+            staThread.Start();
+            staThread.Join();
+            if (threadEx != null)
+            {
+                throw threadEx;
+            }
+
+            return text;
+        }
+    }
+}
--- Flow.Launcher.Plugin.Whatsup/Flow.Launcher.Plugin.Whatsup/Main.cs
+++ Flow.Launcher.Plugin.Whatsup/Flow.Launcher.Plugin.Whatsup/Main.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +9,113 @@ namespace Flow.Launcher.Plugin.Whatsup
 {
     public class Main : IPlugin
     {
+        public const string PluginActionKeyworkd = "wh";
+        private readonly IClipboardHelper _clipboardHelper;
+
+        // @"https://web.whatsapp.com/send?phone=972544325740";
+        private readonly string webApistring = @"https://web.whatsapp.com/send?phone=972";
+
+        public Main()
+        {
+            _clipboardHelper ??= new ClipboardHelper();
+        }
+
+        public Main(IClipboardHelper clipboardHelper) : this()
+        {
+            _clipboardHelper = clipboardHelper;
+        }
+
         public List<Result> Query(Query query)
         {
-            throw new NotImplementedException();
+            List<Result> list = new();
+
+            var queryString = query.Search.Trim();
+            if (queryString == string.Empty)
+            { // try clipboard
+                var clipboard = _clipboardHelper.GetClipboardText();
+                queryString = clipboard;
+            }
+
+            var phoneNumber = TryExtractPhoneNumber(queryString);
+            if (phoneNumber != null)
+            {
+                list.Add(
+                    new Result
+                    {
+                        Score = 100,
+                        IcoPath = "Images\\whatup.png",
+                        Title = $"{phoneNumber}",
+                        SubTitle = $"Send message to {phoneNumber}",
+                        Action = context =>
+                        {
+                            Process.Start(webApistring + queryString[1..]);
+                            return true;
+                        },
+                    });
+            }
+
+            return list;
+        }
+
+        public static string TryExtractPhoneNumber(string phoneString)
+        {
+            if (string.IsNullOrWhiteSpace(phoneString))
+            {
+                return null;
+            }
+
+            foreach (var possibleNumber in GetNumbers(phoneString))
+            {
+                if (possibleNumber.Length == 12 && possibleNumber.StartsWith("972", StringComparison.OrdinalIgnoreCase))
+                {
+                    return "0" + possibleNumber.Substring(3);
+                }
+
+                if (possibleNumber.Length == 10 && possibleNumber.StartsWith("05"))
+                {
+                    return possibleNumber;
+                }
+            }
+
+            return null;
+        }
+
+        public static List<string> GetNumbers(string query)
+        {
+            var result = new List<string>();
+            var builder = new StringBuilder();
+
+            foreach (var ch in query)
+            {
+                if (char.IsDigit(ch))
+                {
+                    builder.Append(ch);
+                }
+                else if (ch != '-' && ch != '+')
+                {
+                    AddFromBuilder();
+                }
+            }
+
+            AddFromBuilder();
+
+            return result;
+
+            void AddFromBuilder()
+            {
+                if (builder.Length <= 0) return;
+                var res = builder.ToString();
+                if (!string.IsNullOrWhiteSpace(res))
+                {
+                    result.Add(res);
+                }
+
+                builder.Clear();
+            }
         }
 
         public void Init(PluginInitContext context)
         {
-            throw new NotImplementedException();
         }
     }
 }
